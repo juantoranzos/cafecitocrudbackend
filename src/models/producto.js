3ba98e487da41907e2dd69a80c9692081ea86e6f
Juanchi <toranzos628@gmail.com>
2023-07-04 17:27:04 -0300

    se creo el modelo base de datos

---
+++ src/models/producto.js
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+const productoScheme = new Schema({
+    nombreProducto: {
+        type: String,
+        minLength: 2,
+        maxLength: 100,
+        unique : true,
+        required: true
+
+    },
+    precio: {
+        type: Number,
+        min: 1,
+        max: 10000,
+        required: true
+    },
+    imagen: {
+        type: String,
+        required: true
+    },
+    categoria:{
+        type: String,
+        required:true
+    }
+})
+const Producto = model('producto', productoScheme);
+export default Producto;
