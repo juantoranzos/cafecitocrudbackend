68c482296795ce4225dae1a83d5b96badfeb9a02
Juanchi <toranzos628@gmail.com>
2023-07-05 18:20:12 -0300

    agregamos las validaciones en el nombre del producto y el precio

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -9,6 +9,22 @@ const router = Router();
 //     res.send('esto es una prueba de la peticion GET a mi backend')
 //   })
 
-router.route('/productos').get(obtenerProductos).post([check("nombreProducto").notEmpty().withMessage('el nombre del producto es obligatorio')],crearProducto)
+router.route('/productos').get(obtenerProductos).post([check("nombreProducto")
+.notEmpty()
+.withMessage('el nombre del producto es obligatorio')
+.isLength({min: 2, max: 100})
+.withMessage('El nombre del producto debe contener entre 2 y 100 caracteres inclusive'), check('precio')
+.notEmpty()
+.withMessage('el precio es un dato obligatorio')
+.isNumeric()
+.withMessage('el precio debe ser un numero') 
+.custom((value)=>{
+    if(value >= 1 && value <=10000){
+        return true
+    }else{
+     throw new Error('El precio debe estar entre 1 y 10000')
+    }
+})],
+ crearProducto)
 router.route('/productos/:id').delete(borrarProducto).put(editarProducto).get(obtenerProducto);
 export default router;
